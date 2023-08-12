ace4a7400e8d3b05988a44752a757d72f8876061
bosqidotes <rizki.10120026@mahasiswa.unikom.ac.id>
2023-08-12 14:27:48 +0700

    initial commit

---
+++ app/src/main/java/com/rizki/uts__10120026/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rizki.uts__10120026;
+//25 mei 2023, 10120026, Rizki Ramdani , IF1
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/rizki/uts__10120026/main/diary/SaveCategoryActivity.java
@@ -0,0 +1,57 @@
+package com.rizki.uts__10120026.main.diary;
+//25 mei 2023, 10120026, Rizki Ramdani , IF1
+
+
+import android.annotation.SuppressLint;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rizki.uts__10120026.R;
+import com.rizki.uts__10120026.model.DBHelper;
+import com.rizki.uts__10120026.model.DBHelper;
+
+
+public class SaveCategoryActivity extends AppCompatActivity {
+    private EditText titleInput;
+    private EditText contentInput;
+    private Button btn_simpan, btn_kembali;
+    DBHelper dbHelper;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_category);
+
+        dbHelper = new DBHelper(this);
+        titleInput = findViewById(R.id.input_title);
+        contentInput = findViewById(R.id.input_content);
+        btn_simpan = findViewById(R.id.button_submit);
+        btn_kembali = findViewById(R.id.button_back);
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("insert into kategori(kategori) values('" + titleInput.getText().toString() + "')");
+                Toast.makeText(getApplicationContext(), "Kategori Berhasil Dibuat", Toast.LENGTH_LONG).show();
+                CategoriActivity.ma.RefreshList();
+                finish();
+            }
+        });
+
+        btn_kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+}
